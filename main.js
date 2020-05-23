b70929e841435507eec9bbe4ff3f9b90c08fef52
zexman <zexman@outlook.com.br>
2020-05-23 13:48:09 -0300

    include a form to load a url

--- main.js
+++ main.js
@@ -1,27 +1,70 @@
-const { app, BrowserWindow } = require("electron");
+const { app, BrowserWindow, globalShortcut, ipcMain } = require("electron");
+const prompt = require("electron-prompt");
+
+let win, winURL;
 
 function createWindow() {
   // Cria uma janela de navegação.
-  const win = new BrowserWindow({
+  win = new BrowserWindow({
     width: 800,
     height: 600,
-    titleBarStyle: "hidden",
-    frame: false,
-    //transparent: true,
+    //titleBarStyle: "hidden",
+    //frame: false,
+    autoHideMenuBar: true,
+    alwaysOnTop: true,
 
     webPreferences: {
       nodeIntegration: true,
     },
   });
 
-  // and load the index.html of the app.
+  //win.webContents.openDevTools();
+
+  // Attach event listener to event that requests to update something in the second window
+  // from the first window
+  ipcMain.on("requestURl", (event, arg) => {
+    // Request to update the label in the renderer process of the second window
+    // and load the index.html of the app.
+    winURL.close();
+    win.loadURL(arg.url);
+  });
+
   win.loadFile("index.html");
+}
+
+function toggleDevTools() {
+  win.webContents.toggleDevTools();
+}
+
+function createShortcuts() {
+  globalShortcut.register("CmdOrCtrl+J", toggleDevTools);
+  globalShortcut.register("CmdOrCtrl+l", createChildWindow);
+}
+
+function createChildWindow() {
+  winURL = new BrowserWindow({
+    parent: win,
+    width: 300,
+    height: 40,
+    //titleBarStyle: "hidden",
+    //frame: false,
+    autoHideMenuBar: true,
+    webPreferences: {
+      nodeIntegration: true,
+    },
+  });
+  //child.webContents.openDevTools();
+  winURL.loadFile("url.html");
 }
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Algumas APIs podem ser usadas somente depois que este evento ocorre.
-app.whenReady().then(createWindow);
+app
+  .whenReady()
+  .then(createWindow)
+  //.then(createChildWindow)
+  .then(createShortcuts);
 
 // Quit when all windows are closed.
 app.on("window-all-closed", () => {

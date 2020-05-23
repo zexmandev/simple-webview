3fef448632858903d5057e945ee8ed245593ee94
zexman <zexman@outlook.com.br>
2020-05-23 09:56:15 -0300

    remove dev tools

--- main.js
+++ main.js
@@ -12,9 +12,6 @@ function createWindow() {
 
   // and load the index.html of the app.
   win.loadFile("index.html");
-
-  // Open the DevTools.
-  win.webContents.openDevTools();
 }
 
 // This method will be called when Electron has finished
